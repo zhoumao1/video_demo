fc6377ef2b83e374571f26b3c934309c6697abd2
zhoumao1 <2445036181@qq.com>
2019-05-24 11:32:48 +0800

    懒加载

--- public/js/classification.js
+++ public/js/classification.js
@@ -7,10 +7,84 @@ $(function () {
 		},
 		success: function (ret) {
 			console.table(ret);
-			var tempCont = $('#movie-temp').html();
-			var htmlText = _.template(tempCont);
-			var NewHtml = htmlText({data: ret});
-			$("#movie-row").html(NewHtml);
+
+			fn(ret)
+			
+			// var tempCont = $('#movie-temp').html();
+			// var htmlText = _.template(tempCont);
+			// var NewHtml = htmlText({data: ret});
+			// $("#movie-row").html(NewHtml);
 		}
 	})
+
+	function fn(arr) {
+		// var st = '';
+		// $.each(arr, function (index, value) {
+		// 	st += '<img datasrc=' + arr[index] + '>'
+		// })
+		// $('.content').html(st)
+
+		var tempCont = $('#movie-temp').html();
+		var htmlText = _.template(tempCont);
+		var NewHtml = htmlText({data: arr});
+		$("#movie-row").html(NewHtml);
+		
+
+		// var wap = $('.main');
+		var offTop = function (ele) {
+			var top = null;
+			var eleTop = ele.offsetTop;
+			var eleParent = ele.offsetParent;
+			top += eleTop;
+			while (eleParent) {
+				top += eleParent.offsetTop;
+				eleParent = eleParent.offsetParent;
+			}
+			return top;
+		}
+
+		function loadimg(img) {
+
+			var winTop = $(window).height() + $(window).scrollTop();
+			var imgTop = offTop(img) + img.offsetHeight;
+			if (winTop > imgTop) {
+				if (img.isload == true) {
+					return
+				}
+				var imgs = new Image;
+				imgs.src = img.getAttribute("data-src")
+				imgs.onload = function () {
+					img.src = this.src;
+				}
+				img.isload = true;
+			}
+		}
+
+
+		var imgs = $('img');
+		console.log(imgs);
+		function allload() {
+
+			$.each(imgs, function (index, value) {
+
+				if (imgs[index].isload) {
+					return;
+				}
+
+				loadimg(imgs[index])
+			})
+		}
+
+		setTimeout(function () {
+			allload()
+		}, 1000)
+
+		$(window).scroll(function () {
+			allload()
+		})
+	}
+
+
+	
+	
 })
